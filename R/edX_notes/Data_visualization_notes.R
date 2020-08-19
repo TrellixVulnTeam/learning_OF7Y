d00570e73025fcbfe014bd47aa19c121cf3f8b9b
Peter Williams <petercw94@gmail.com>
2020-08-19 15:15:16 -0600

    more notes on probability and visualization

--- R/edX_notes/Data_visualization_notes.R
+++ R/edX_notes/Data_visualization_notes.R
@@ -142,7 +142,153 @@ p2 <- qplot(x,y)
 grid.arrange(p1, p2, ncol = 2)
 
 
-# summarizing with dplyer
+# summarizing with dplyer ####
+# the summarize functions allows for quick descriptive stats analysis of data sets
+s = heights %>% filter(sex == "Male") %>%
+  summarize(average = mean(height), standard_deviation = sd(height))
+
+# this returns a data frame. However, sometimes it can be useful to have a vector instead of a df.
+# To have dplyer functions return vectors, try this:
+us_murder_rate <- murders %>%
+  summarize(rate = sum(total) / sum(population * 100000)) %>%
+  .$rate
+
+# moving on with the summarize function. We can group the data before summarizing to make it more
+# observable:
+heights %>%
+  group_by(sex) %>%
+  summarize(average = mean(height), standard_deviation = sd(height))
+
+murders <- murders %>%
+  mutate(murder_rate = total/population * 100000)
+
+murders %>%
+  group_by(region) %>%
+  summarize(median_rate = median(murder_rate))
 
 
+# gapminder case study ####
+data("gapminder")
+head(gapminder)
 
+summary(gapminder)
+# which country had the highest child mortality rate?
+gapminder %>%
+  group_by(country) %>%
+  summarize(infant_mortality = mean(infant_mortality, na.rm = TRUE))
+
+# facet wrapping
+ggplot(gapminder, aes(fertility, life_expectancy, color = continent)) +
+  geom_point() + facet_wrap(~gapminder$year)
+
+# however, this has too many variables to be easily interpreted
+years <- c(1962, 1980, 1990, 2000, 2012)
+continents <- c("Europe", "Asia")
+gapminder %>%
+  filter(year %in% years & continent %in% continents) %>%
+  ggplot(aes(fertility, life_expectancy, col = continent)) +
+  geom_point() +
+  facet_wrap(~year)
+
+# another useful facet techniques is facet_grid:
+filter(gapminder, year %in% c(1962, 2012)) %>%
+  ggplot(aes(fertility, life_expectancy, col = continent)) +
+  geom_point() +
+  facet_grid(continent ~ year)
+
+# however, this has broken our graph into two variables, continent and year.
+# columns - year
+# rows - continent
+# to fix this, we can use the dot:
+filter(gapminder, year %in% c(1962, 2012)) %>%
+  ggplot(aes(fertility, life_expectancy, col = continent)) +
+  geom_point() +
+  facet_grid(. ~ year)
+
+# timeseries ####
+
+# the following plot will allow us to look at the fertility rate over time by country
+gapminder %>% 
+  ggplot(aes(x = year, y = fertility)) +
+  geom_line() + facet_wrap(~country)
+
+# obviously this plot isn't a good visual aid for presentation, but it can be very useful in helping
+# the analyst quickly pick out trends between countries.
+
+# if we want to follow this trend for a specific country, we can filter for it.
+gapminder %>%
+  filter(country == "United States") %>%
+  ggplot(aes(year, fertility)) +
+  geom_line()
+
+# if we want more than one country:
+countries <- c("South Korea", "Germany")
+gapminder %>% filter(country %in% countries) %>%
+  ggplot(aes(year, fertility)) +
+  geom_line()
+
+# this however, is not what we want. This line goes through the points of each graph. 
+# if we want two seperate lines, we have to group our data in the aesthetics
+gapminder %>% filter(country %in% countries) %>%
+  ggplot(aes(year, fertility, group = country)) +
+  geom_line()
+
+# if we want to add in labels to this plot we can do this:
+labels <- data.frame(country = countries, x = c(1975, 1965), y = c(60, 72))
+gapminder %>% filter(country %in% countries) %>%
+  ggplot(aes(year, life_expectancy, col = country)) +
+  geom_line() +
+  geom_text(data = labels, aes(x, y, label = country), size = 5) +
+  theme(legend.position = "none")
+
+# Data Transformations ####
+
+# The gapminder data table includes a column with the countries’ gross domestic product (GDP). 
+# GDP measures the market value of goods and services produced by a country in a year. 
+# The GDP per person is often used as a rough summary of a country’s wealth. Here we divide 
+# this quantity by 365 to obtain the more interpretable measure dollars per day. 
+# Using current US dollars as a unit, a person surviving on an income of less than $2 a day is 
+# defined to be living in absolute poverty. We add this variable to the data table:
+
+gapminder <- gapminder %>%  mutate(dollars_per_day = gdp/population/365) # dividing gdp gives dollars per year per person. Dividing by 365 gives dollars per day
+
+# here is a histogram of per day incomes
+past_year <- 1970
+gapminder %>% 
+  filter(year == past_year & !is.na(gdp)) %>% # make sure to filter out any NAs
+  ggplot(aes(dollars_per_day)) + 
+  geom_histogram(binwidth = 1, color = "black")
+
+
+# It might be more informative to quickly be able to see how many countries have average daily 
+# incomes of about $1 (extremely poor), $2 (very poor), $4 (poor), $8 (middle), $16 (well off), 
+# $32 (rich), $64 (very rich) per day. These changes are multiplicative and log transformations 
+# convert multiplicative changes into additive ones: when using base 2, a doubling of a value turns 
+# into an increase by 1.
+gapminder %>% 
+  filter(year == past_year & !is.na(gdp)) %>%
+  ggplot(aes(log2(dollars_per_day))) + 
+  geom_histogram(binwidth = 1, color = "black")
+  
+# In the dollars per day example, we used base 2 instead of base 10 because the resulting range is easier to interpret. The range of the values being plotted is 0.327, 48.885.
+
+# In base 10, this turns into a range that includes very few integers: just 0 and 1. 
+# With base two, our range includes -2, -1, 0, 1, 2, 3, 4, and 5. It is easier to compute 2^x
+# and 10^x when x is an integer and between -10 and 10, so we prefer to have smaller integers in 
+# the scale. Another consequence of a limited range is that choosing the binwidth is more 
+# challenging. With log base 2, we know that a binwidth of 1 will translate to a bin with range x
+# to 2x
+
+# transform the values or the scales? ####
+
+# the previous plot used logged values. This is what scaled logs looks like:
+gapminder %>% 
+  filter(year == past_year & !is.na(gdp)) %>%
+  ggplot(aes(dollars_per_day)) + 
+  geom_histogram(binwidth = 1, color = "black") +
+  scale_x_continuous(trans = "log2")
+
+# scaled logs allow us to quickly see the value on the x axis, rather than reading 5 log 2 dollars per day
+
+  
+  
--- R/edX_notes/Probability.R
+++ R/edX_notes/Probability.R
@@ -1,3 +1,6 @@
+
+# Discrete Probability ####
+
 # Monte Carlo simulation for picking a red or blue bead out of an urn
 # creating the urn with 5 total beads: 2 red, three blue
 beads = rep(c("red", "blue"), times = c(2,3))
@@ -8,10 +11,56 @@ sample(beads, 1)
 
 # if we run this sample an infinite number of times, we can determine all the possible outcomes and 
 # their accompanying probablilities
-# Hoever, it is impossible to run it an infinite number of times, so we run it enough times we can
+# However, it is impossible to run it an infinite number of times, so we run it enough times we can
 # simulate the results of running it an infinite amount of times. This is called the monte carlo affect
-while(x < 1000){
-  x = 0
-  sample(beads, 1)
-  x = x + 1
-}
+B <- 10000
+events = replicate(B, sample(beads, 1))
+# here we performed our experiment from before but 10,000 times
+
+# in order to count the number of outcomes for each color, we can build a contingency 
+# table:
+tab = table(events)
+tab
+
+# prop.table() gives us the proportion of each event:
+prop.table(tab)
+
+# another way to find the probablity of tables or events is with the mean funciton:
+mean(events == "blue")
+
+# Monte Carlo simulations ####
+
+# setting the seed can help generate the same random variable each time the code is run.
+# This helps to prevent getting different answers everytime you run the same piece of code
+# and avoids confusion.
+# Choosing the seed to set is conventionally done by subtracting the current year by the
+# day and month
+set.seed(1986)
+
+# with and without replacement 
+# the function sample() automatically sets the argument "replace" to FALSE. In other words
+# when you take a bead out of the urn, it does not get replaced (dependent events)
+
+# Independence ####
+# two events are independent if the outcome of one does not affect the other (coin toss)
+# two events are dependent if the outcome of one DOES affect the other. If we deal a King
+# for the first card, the probability of getting a king for the second card because there
+# are now only three kings instead of 4.
+
+# as far as notation:
+# for independent:
+#   Pr(A | B) = Pr(A) (the probability of A given B = probability of A)
+
+# for dependent:
+#   Pr(A | B)
+
+# multiplication rule for independent events: ####
+#  Pr(A and B and C) = Pr(A) * Pr(B) * Pr(C)
+
+# multiplication rule for dependent events: ####
+#  Pr(A and B) = Pr(A) * Pr(B | A)
+# for more than two events:
+#  Pr(A and B and C) = Pr(A) * Pr(B | A) * Pr(C | A and B)
+
+
+
